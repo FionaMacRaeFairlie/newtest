c7e68d7b0162bd3f8639ec671c08dfa3327e0385
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2024-11-08 09:16:05 +0000

    update todo example

--- src/components/todo/Todoitem.js
+++ src/components/todo/Todoitem.js
@@ -1,19 +1,39 @@
-import React, { Component } from "react";
-class TodoItem extends Component {
-    render() {
-      const { deleteTodo, todo } = this.props;
-  
-      return (
-        <div className="TodoItem" data-testid="TodoItem">
-          <div data-testid="TodoItem-priority" className={`TodoItem-priority TodoItem--${todo.priority}`} />
-          <div data-testid="TodoItem-text" className={`TodoItem-text`}>
-            {todo.text}
-          </div>
-          <button onClick={() => deleteTodo(todo)} className="TodoItem-delete">
-            -
-          </button>
-        </div>
-      );
-    }
-  }
-  export default TodoItem
+// import React, { Component } from "react";
+// class TodoItem extends Component {
+//     render() {
+//       const { deleteTodo, todo } = this.props;
+
+//       return (
+//         <div className="TodoItem" data-testid="TodoItem">
+//           <div data-testid="TodoItem-priority" className={`TodoItem-priority TodoItem--${todo.priority}`} />
+//           <div data-testid="TodoItem-text" className={`TodoItem-text`}>
+//             {todo.text}
+//           </div>
+//           <button onClick={() => deleteTodo(todo)} className="TodoItem-delete">
+//             -
+//           </button>
+//         </div>
+//       );
+//     }
+//   }
+//   export default TodoItem
+
+import React from "react";
+
+export default function TodoItem(props) {
+  const { deleteTodo, todo } = props;
+  return (
+    <div className="TodoItem" data-testid="TodoItem">
+      <div
+        data-testid="TodoItem-priority"
+        className={`TodoItem-priority TodoItem--${todo.priority}`}
+      />
+      <div data-testid="TodoItem-text" className={`TodoItem-text`}>
+        {todo.text}
+      </div>
+      <button onClick={() => deleteTodo(todo)} className="TodoItem-delete">
+        -
+      </button>
+    </div>
+  );
+}
--- src/components/todo/Todolist.js
+++ src/components/todo/Todolist.js
@@ -1,9 +1,22 @@
-import React, { Component } from "react";
-import TodoItem from './Todoitem'
-class TodoList extends Component {
-    render() {
-      const { todos, deleteTodo } = this.props;
-      return todos.map((todo, i) => <TodoItem deleteTodo={deleteTodo} key={i} todo={todo} />);
-    }
-  }
-  export default TodoList
+// import React, { Component } from "react";
+import React from "react";
+import TodoItem from "./Todoitem";
+// class TodoList extends Component {
+//     render() {
+//       const { todos, deleteTodo } = this.props;
+//       return todos.map((todo, i) => <TodoItem deleteTodo={deleteTodo} key={i} todo={todo} />);
+//     }
+//   }
+//   export default TodoList
+
+export default function TodoList(props) {
+  const { todos, deleteTodo } = props;
+
+  return (
+    <div>
+      {todos.map((todo, i) => (
+        <TodoItem deleteTodo={deleteTodo} key={i} todo={todo} />
+      ))}
+    </div>
+  );
+}
